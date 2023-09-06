0aa928e0ea13aed0c295c9879e55c1d57638db51
azagupta <116111865+azagupta@users.noreply.github.com>
2023-09-06 13:05:47 +0100

    Update home.py

--- home.py
+++ home.py
@@ -10,15 +10,3 @@ def app():
 	
 
 
-# S6.1: Design the View Data page of the multipage app.
-# Import necessary modules
-import numpy as np
-import pandas as pd
-import streamlit as st
-
-# Define a function 'app()' which accepts 'car_df' as an input.
-def app(car_df):
-    st.header("View Data")
-    # Add an expander and display the dataset as a static table within the expander.
-    with st.beta_expander("View Dataset"):
-        st.table(car_df)
